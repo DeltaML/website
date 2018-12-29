603f23a62a512435757cca2d0ed5564b7174c6ce
Agustin Rojas <agustin.rojas@despegar.com>
2018-12-29 13:26:08 -0300

    Add initial files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from gevent.pywsgi import WSGIServer
+from flask import render_template
+
+app = Flask(__name__, static_url_path='', 
+            static_folder='./assets',
+            template_folder='./templates')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    
+    server = WSGIServer(('0.0.0.0', 5000), app, log=app.logger)
+    server.serve_forever()
