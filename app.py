e51dc5713d1abc1c9dcf63bba39cf32734892bdd
Agustin Rojas <agustin.rojas@despegar.com>
2018-12-29 13:44:33 -0300

    Fix PORT

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 from flask import render_template
-
+DEFAULT_PORT = 5000
 app = Flask(__name__, static_url_path='', 
             static_folder='./assets',
             template_folder='./templates')
@@ -10,7 +11,13 @@ app = Flask(__name__, static_url_path='',
 def index():
     return render_template('index.html')
 
+
+def get_port():
+    env_port = os.environ['PORT']
+    return int(env_port) if env_port else DEFAULT_PORT
+
 if __name__ == "__main__":
     
-    server = WSGIServer(('0.0.0.0', 5000), app, log=app.logger)
+    port = get_port()
+    server = WSGIServer(('0.0.0.0', port), app, log=app.logger)
     server.serve_forever()
